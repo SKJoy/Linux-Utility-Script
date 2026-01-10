0986e1bed2f6e079c5a08c946897de0376a4c962
Broken Arrow <SKJoy2001@GMail.Com>
2026-01-11 01:09:50 +0600

    Test

---
+++ Set-default-editor-Nano.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Load common library
+source /Joy/Utility/Script/Common.sh
+
+# Set working path to this script's path
+cd $SCRIPT_PATH
+
+# Write your code below ---
+
+if [[ "$EDITOR" == "nano" ]]; then
+	echo "- Nano is already the default editor"
+else
+	FILE_TO_EDIT="~/.profile"
+
+	if [ -f "$FILE_TO_EDIT" ]; then
+		# FILE_TO_EDIT="~/.profile"
+	else
+		FILE_TO_EDIT="~/.bashrc"
+	fi
+fi
+
+# cat <<CONTENT >> $FILE_TO_EDIT
+# export EDITOR="nano"
+# export VISUAL="$EDITOR"
+# CONTENT
+
+# Show result
+cat <<CONTENT
+
+- FILE_TO_EDIT = "${FILE_TO_EDIT}"
+
+CONTENT
