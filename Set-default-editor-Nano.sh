edd82f4a9d7a28a7a3ba7286711f1b5039e634e5
Broken Arrow <SKJoy2001@GMail.Com>
2026-01-11 01:38:36 +0600

    Set Nano as default CLI editor

--- Set-default-editor-Nano.sh
+++ Set-default-editor-Nano.sh
@@ -8,15 +8,13 @@ cd $SCRIPT_PATH
 
 # Write your code below ---
 
-FILE_TO_EDIT="~/.profile"
+FILE_TO_EDIT=~/.bashrc
 
 if [[ "$EDITOR" == "nano" ]]; then
 	echo "- Nano is already the default editor"
 else
-	if [ -f "$FILE_TO_EDIT" ]; then
-		X="Y"
-	else
-		FILE_TO_EDIT="~/.bashrc"
+	if [ ! -f $FILE_TO_EDIT ]; then
+		FILE_TO_EDIT=~/.profile
 	fi
 
 	echo "export EDITOR=\"nano\"" >> $FILE_TO_EDIT
