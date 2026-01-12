beb05b972c4f7ba6ae6088ed2641ada80e2c312d
Broken Arrow <SKJoy2001@GMail.Com>
2026-01-13 02:02:25 +0600

    Update

--- OS-Update.sh
+++ OS-Update.sh
@@ -9,20 +9,20 @@ cd $SCRIPT_PATH
 # Write your code below ---
 
 if [[ "$LINUX_DISTRIBUTION" == $LINUX_DISTRIBUTION_ALPINE ]]; then
-	apk add sudo curl wget nano htop mc git
+	apk add sudo openssl curl wget nano htop mc git
 	apk update
 	apk upgrade
 else
 	if [[ "$LINUX_DISTRIBUTION" == $LINUX_DISTRIBUTION_DEBIAN ]]; then
 		apt-get clean
-		apt-get -y install sudo curl wget nano htop mc git
+		apt-get -y install sudo openssl curl wget nano htop mc git
 		apt-get update
 		apt-get -y upgrade
 	else
 		if [[ "$LINUX_DISTRIBUTION" == $LINUX_DISTRIBUTION_RHEL ]]; then
 			dnf clean all
 			dnf -y install epel-release
-			dnf -y install sudo curl wget nano htop mc git
+			dnf -y install sudo openssl curl wget nano htop mc git
 			dnf update
 			dnf upgrade -y
 		else
