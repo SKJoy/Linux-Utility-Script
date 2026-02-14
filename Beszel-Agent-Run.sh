c9977b5b59632ba3d46247fff8a8cf038543c4a8
Broken Arrow <SKJoy2001@GMail.Com>
2026-02-14 06:19:11 +0600

    Chnage to native Linux binary installation

--- Beszel-Agent-Run.sh
+++ Beszel-Agent-Run.sh
@@ -11,21 +11,24 @@ cd $SCRIPT_PATH
 PUBLIC_KEY=$1
 TOKEN=$2
 HUB_DOMAIN=$3
-CONTAINER_NAME="beszel-agent"
 
-docker container stop ${CONTAINER_NAME}
-docker container rm ${CONTAINER_NAME}
+curl -sL https://get.beszel.dev -o /tmp/install-agent.sh && chmod +x /tmp/install-agent.sh && /tmp/install-agent.sh -p 45876 -k "${PUBLIC_KEY}" -t "${TOKEN}" -url "${HUB_DOMAIN}"
 
-docker run -d \
-  --name ${CONTAINER_NAME} \
-  --restart unless-stopped \
-  -e KEY="${PUBLIC_KEY}" \
-  -e LISTEN=45876 \
-  -e TOKEN="${TOKEN}" \
-  -e HUB_URL="https://${HUB_DOMAIN}" \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  -p 45876:45876 \
-  henrygd/beszel-agent
+# CONTAINER_NAME="beszel-agent"
+
+# docker container stop ${CONTAINER_NAME}
+# docker container rm ${CONTAINER_NAME}
+
+# docker run -d \
+#   --name ${CONTAINER_NAME} \
+#   --restart unless-stopped \
+#   -e KEY="${PUBLIC_KEY}" \
+#   -e LISTEN=45876 \
+#   -e TOKEN="${TOKEN}" \
+#   -e HUB_URL="https://${HUB_DOMAIN}" \
+#   -v /var/run/docker.sock:/var/run/docker.sock:ro \
+#   -p 45876:45876 \
+#   henrygd/beszel-agent
 
 # Show result
 cat <<CONTENT
