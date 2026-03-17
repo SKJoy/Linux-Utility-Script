c4699a8d38e9b8a03b1c074d8c2c70c58499e930
Broken Arrow <SKJoy2001@GMail.Com>
2026-03-18 03:57:31 +0600

    WordPress Restore updated

--- WordPress-Restore.sh
+++ WordPress-Restore.sh
@@ -8,18 +8,16 @@ source /Joy/Utility/Script/Common.sh
 
 # Write your code below ---
 
-echo "Backup file name (do not include '.zip') [wordpress]: " && read BFN && BFN=${BFN:="wordpress"} && unzip -o $BFN.zip`
-echo "Press ENTER to edit 'wp-config.php' to set new database credentials..." && read DUMMY_INPUT && nano wp-config.php
-
-echo "Old domain: " && read OU && echo "New domain [old domain]: " && read NU && NU=${NU:="${OU}"} && echo "Migrating... please wait" && wp db import wordpress.sql && rm -f wordpress.sql && wp option update siteurl "https://${NU}" && wp option update home "https://${NU}" && wp search-replace "https://${OU}" "https://${NU}" --skip-columns=guid
+BFN=$1
+unzip -o $BFN.zip` && echo "Press ENTER to edit 'wp-config.php' to set new database credentials..." && read DUMMY_INPUT && nano wp-config.php && wp db import wordpress.sql && rm -f wordpress.sql
 
 # Show result
 cat <<CONTENT
 
 Usage: bash $0
+- BACKUP_FILE = "backup.zip"
 
 Note
-"- Backup file = ${BFN}.zip"
 "- WordPress CLI must be installed"
 
 CONTENT
